e18b38aa5d7992a088fdf3bee809a4a50bae5c2d
anigvo <anigvo@gmail.com>
2023-11-20 21:30:13 +0200

    update

--- src/js/checkPath.js
+++
@@ -1,9 +0,0 @@
-const currentPath = window.location.pathname;
-
-if (
-  currentPath !== '/index.html' &&
-  currentPath !== '/eng.html' &&
-  currentPath !== '/'
-) {
-  window.location.href = '/';
-}
